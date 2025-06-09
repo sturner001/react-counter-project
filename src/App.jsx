8d4c200dc29364262fe69356da3d7e82a9b3e307
Sam Turner <sam.turner@kokadjo.net>
2025-06-09 12:54:54 -0400

    react optimization techniques

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,23 @@
 import { useState } from 'react';
 
 import Counter from './components/Counter/Counter.jsx';
+import ConfigCounter from './components/Counter/ConfigCounter.jsx';
 import Header from './components/Header.jsx';
 import { log } from './log.js';
 
 function App() {
   log('<App /> rendered');
 
-  const [enteredNumber, setEnteredNumber] = useState(0);
   const [chosenCount, setChosenCount] = useState(0);
 
-  function handleChange(event) {
-    setEnteredNumber(+event.target.value);
+  function handleSetCount(newCount) {
+    setChosenCount(newCount);
   }
-
-  function handleSetClick() {
-    setChosenCount(enteredNumber);
-    setEnteredNumber(0);
-  }
-
   return (
     <>
       <Header />
       <main>
-        <section id="configure-counter">
-          <h2>Set Counter</h2>
-          <input type="number" onChange={handleChange} value={enteredNumber} />
-          <button onClick={handleSetClick}>Set</button>
-        </section>
+        <ConfigCounter onSet={handleSetCount} />
         <Counter initialCount={chosenCount} />
       </main>
     </>
